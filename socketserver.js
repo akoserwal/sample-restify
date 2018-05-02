13c1bc5fd9d0d1c078d4bf90017672109e4561a1
Abhishek Koserwal <akoserwa@redhat.com>
2018-05-02 18:59:39 +0530

    Initial commit

---
+++ socketserver.js
@@ -0,0 +1,34 @@
+var restify = require('restify');
+var socketio = require('socket.io');
+var fs = require('fs');
+var server = restify.createServer();
+var io = socketio.listen(server.server);
+
+server.get('/', function indexHTML(req, res, next) {
+    fs.readFile(__dirname + '/index.html', function (err, data) {
+        if (err) {
+            next(err);
+            return;
+        }
+
+        res.setHeader('Content-Type', 'text/html');
+        res.writeHead(200);
+        res.end(data);
+        next();
+    });
+});
+
+
+
+
+io.sockets.on('connection', function (socket) {
+    
+    socket.emit('news', { hello: 'world' });
+    socket.on('my other event', function (data) {
+            console.log(data);
+    });
+});
+
+server.listen(8084, function () {
+    console.log('socket.io server listening at %s', server.url);
+});
